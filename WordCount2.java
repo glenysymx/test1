6860d0cd0dfd07acd5fa717e32b028ce1e5c35ec
glenysymx <73132165+glenysymx@users.noreply.github.com>
2021-11-17 16:03:34 +0000

    Update WordCount2.java

--- WordCount2.java
+++ WordCount2.java
@@ -1,94 +1,89 @@
-import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.StringTokenizer;
- 
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.FileUtil;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
-import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
-import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
- 
-public class Ngramstatistics {
-	public static class Map1 extends Mapper<longwritable, text,="" intwritable=""> {
-		private final static IntWritable one = new IntWritable(1);
-		private Text word = new Text();
-		static int cnt = 0;
-		List ls = new ArrayList();
-		@SuppressWarnings("unchecked")
-		public void map(LongWritable key, Text value, Context context)
-				throws IOException, InterruptedException {
-			StringTokenizer dt = new StringTokenizer(value.toString(), " ");
-			
-			while (dt.hasMoreTokens()) {
-				ls.add(dt.nextToken());
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class WordCount {
+
+  public static class WCMapper
+       extends Mapper<Object, Text, Text, IntWritable>{
+
+    private final static IntWritable one = new IntWritable(1);
+    private Text word = new Text();
+    static int cnt = 0;
+    List ls = new ArrayList();
+    @SuppressWarnings("unchecked")
+    public void map(Object key, Text value, Context context
+                    ) throws IOException, InterruptedException {
+      StringTokenizer itr = new StringTokenizer(value.toString(), " ");
+	    
+
+      //Tokenised strings contain punctuations, e.g. comma, and fullstop
+      //Your task is to ensure that only words themselves are used as keys
+
+      while (itr.hasMoreTokens()) {
+	ls.add(itr.nextToken());
+      }
+    }
+    @Override
+    protected void cleanup(Context context) throws IOException, InterruptedException {
+    	StringBuffer str = new StringBuffer("");
+	for (int i = 0; i < ls.size() - 3; i++) {
+		int k=i;
+		for(int j=0;j<3;j++) {
+			if(j>0) {
+				str = str.append(" ");
+				str = str.append(ls.get(k));
+			} else 
+			{
+				str = str.append(ls.get(k));
 			}
-		
+			k++;
 		}
-		@Override
-	    protected void cleanup(Context context) throws IOException, InterruptedException {
-			int va = Integer.parseInt(context.getConfiguration().get("grams"));
-			StringBuffer str = new StringBuffer("");
-			for (int i = 0; i < ls.size() - va; i++) {
-				int k=i;
-				for(int j=0;j<va;j++) {="" if(j="">0) {
-						str = str.append(" ");
-						str = str.append(ls.get(k));
-						
-					} else 
-					{
-						str = str.append(ls.get(k));
-						
-					}
-					k++;
-				}
-				word.set(str.toString());
-				str=new StringBuffer("");
-				//one.set(ls.size());
-				context.write(word, one);
-			}
-	    }
+		word.set(str.toString());
+		str=new StringBuffer("");
+		context.write(word,one);
 	}
- 
-	public static class Reduce1 extends Reducer<text, intwritable,="" text,="" intwritable=""> {
-		public void reduce(Text key, Iterable values,
-				Context context) throws IOException, InterruptedException {
-			int sum = 0;
-			for (IntWritable val : values) {
-				sum += val.get();
-			}
-			context.write(key, new IntWritable(sum));
-		}
-	}
- 
-	public static void main(String[] args) throws Exception {
-		FileUtil.fullyDelete(new File(args[1]));
- 
-		Configuration conf = new Configuration();
-		conf.set("grams", args[2]);
-		Job job = new Job(conf, "wordcount");
-		// job.setNumReduceTasks(0);
-		job.setJarByClass(TreeDriver.class);
-		job.setOutputKeyClass(Text.class);
-		job.setOutputValueClass(IntWritable.class);
-		job.setMapOutputValueClass(IntWritable.class);
-		job.setMapOutputKeyClass(Text.class);
- 
-		job.setInputFormatClass(TextInputFormat.class);
-		job.setOutputFormatClass(TextOutputFormat.class);
-		TextInputFormat.addInputPath(job, new Path(args[0]));
-		TextOutputFormat.setOutputPath(job, new Path(args[1]));
-		job.setMapperClass(Map1.class);
-		job.setReducerClass(Reduce1.class);
-		job.waitForCompletion(true);
-		System.out.println("Done.");
- 
-	}
+    }
+  }
+
+  public static class WCReducer
+       extends Reducer<Text,IntWritable,Text,IntWritable> {
+    private IntWritable result = new IntWritable();
+
+    public void reduce(Text key, Iterable<IntWritable> values,
+                       Context context
+                       ) throws IOException, InterruptedException {
+      int sum = 0;
+      for (IntWritable val : values) {
+        sum += val.get();
+      }
+      result.set(sum);
+      context.write(key, result);
+    }
+  }
+
+  public static void main(String[] args) throws Exception {
+    Configuration conf = new Configuration();
+    Job job = Job.getInstance(conf, "word count");
+    job.setJarByClass(WordCount.class);
+    job.setMapperClass(WCMapper.class);
+    job.setReducerClass(WCReducer.class);
+    job.setOutputKeyClass(Text.class);
+    job.setOutputValueClass(IntWritable.class);
+    FileInputFormat.addInputPath(job, new Path(args[0]));
+    FileOutputFormat.setOutputPath(job, new Path(args[1]));
+    System.exit(job.waitForCompletion(true) ? 0 : 1);
+  }
 }
+
